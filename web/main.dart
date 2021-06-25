9c061b508aaefcabcb385f49c799fa98a560b896
niha.bhaskar <niha.bhaskar@gmail.com>
2021-06-24 17:02:08 -0700

    angular list

--- web/main.dart
+++ web/main.dart
@@ -1,11 +1,14 @@
 import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
-import 'package:angular_tour_of_heroes/app_component.template.dart' as ng;
+import 'package:angular_app/app_component.template.dart' as ng;
+import 'package:angular_app/in_memory_data_service.dart';
+import 'package:http/http.dart';
 
 import 'main.template.dart' as self;
 
 @GenerateInjector(
   routerProvidersHash, // You can use routerProviders in production
+  ClassProvider(Client, useClass: InMemoryDataService),
 )
 final InjectorFactory injector = self.injector$Injector;
 
